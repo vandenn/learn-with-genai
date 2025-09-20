00a2983ca6e07b8c47c1b1a564872f37970e5dab
vandenn <evan.livelo@gmail.com>
2025-09-21 02:08:09 +0800

    Update font to use Open Sans

--- frontend/.next/static/chunks/src_app_page_tsx_445a3400._.js
+++ frontend/.next/static/chunks/src_app_page_tsx_445a3400._.js
@@ -1,7 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/src_app_globals_91e4631d.css"
+  "static/chunks/node_modules_8c5346da._.js",
+  "static/chunks/src_components_61af5646._.js"
 ],
     source: "dynamic"
 });
